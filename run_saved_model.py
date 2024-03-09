af70edc8d018710ecd411a780f28d88673367161
damiad <damian12msi@gmail.com>
2024-03-09 13:57:03 +0100

    chore: repo template

---
+++ run_saved_model.py
@@ -0,0 +1,150 @@
+import argparse
+import torch
+from accelerate import Accelerator, DeepSpeedPlugin
+from accelerate import DistributedDataParallelKwargs
+from torch import nn, optim
+from torch.optim import lr_scheduler
+
+from models import Autoformer, DLinear, TimeLLM
+
+from data_provider.data_factory import data_provider
+import random
+import numpy as np
+import os
+
+os.environ['CURL_CA_BUNDLE'] = ''
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:64"
+
+from utils.tools import vali, load_content
+
+parser = argparse.ArgumentParser(description='Time-LLM')
+
+fix_seed = 2021
+random.seed(fix_seed)
+torch.manual_seed(fix_seed)
+np.random.seed(fix_seed)
+
+# basic config
+parser.add_argument('--task_name', type=str, required=True, default='long_term_forecast',
+                    help='task name, options:[long_term_forecast, short_term_forecast, imputation, classification, anomaly_detection]')
+parser.add_argument('--is_training', type=int, required=True, default=1, help='status')
+parser.add_argument('--model_id', type=str, required=True, default='test', help='model id')
+parser.add_argument('--model_comment', type=str, required=True, default='none', help='prefix when saving test results')
+parser.add_argument('--model', type=str, required=True, default='Autoformer',
+                    help='model name, options: [Autoformer, DLinear]')
+parser.add_argument('--seed', type=int, default=2021, help='random seed')
+
+# data loader
+parser.add_argument('--data', type=str, required=True, default='ETTm1', help='dataset type')
+parser.add_argument('--root_path', type=str, default='./dataset', help='root path of the data file')
+parser.add_argument('--data_path', type=str, default='ETTh1.csv', help='data file')
+parser.add_argument('--features', type=str, default='M',
+                    help='forecasting task, options:[M, S, MS]; '
+                         'M:multivariate predict multivariate, S: univariate predict univariate, '
+                         'MS:multivariate predict univariate')
+parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+parser.add_argument('--loader', type=str, default='modal', help='dataset type')
+parser.add_argument('--freq', type=str, default='h',
+                    help='freq for time features encoding, '
+                         'options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], '
+                         'you can also use more detailed freq like 15min or 3h')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+# forecasting task
+parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+parser.add_argument('--label_len', type=int, default=48, help='start token length')
+parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
+
+# model define
+parser.add_argument('--model_path', type=str, required=True)
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=7, help='output size')
+parser.add_argument('--d_model', type=int, default=16, help='dimension of model')
+parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+parser.add_argument('--d_ff', type=int, default=32, help='dimension of fcn')
+parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+parser.add_argument('--factor', type=int, default=1, help='attn factor')
+parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
+parser.add_argument('--embed', type=str, default='timeF',
+                    help='time features encoding, options:[timeF, fixed, learned]')
+parser.add_argument('--activation', type=str, default='gelu', help='activation')
+parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
+parser.add_argument('--patch_len', type=int, default=16, help='patch length')
+parser.add_argument('--stride', type=int, default=8, help='stride')
+parser.add_argument('--prompt_domain', type=int, default=0, help='')
+
+# optimization
+parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
+parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+parser.add_argument('--align_epochs', type=int, default=10, help='alignment epochs')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+parser.add_argument('--eval_batch_size', type=int, default=8, help='batch size of model evaluation')
+parser.add_argument('--patience', type=int, default=10, help='early stopping patience')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+parser.add_argument('--des', type=str, default='test', help='exp description')
+parser.add_argument('--loss', type=str, default='MSE', help='loss function')
+parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+parser.add_argument('--pct_start', type=float, default=0.2, help='pct_start')
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+parser.add_argument('--llm_layers', type=int, default=6)
+parser.add_argument('--percent', type=int, default=100)
+
+args = parser.parse_args()
+ddp_kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+deepspeed_plugin = DeepSpeedPlugin(hf_ds_config='./ds_config_zero2.json')
+accelerator = Accelerator(kwargs_handlers=[ddp_kwargs], deepspeed_plugin=deepspeed_plugin)
+
+file = open("logs/example.txt", 'w')
+for ii in range(1):
+    train_data, train_loader = data_provider(args, 'train')
+    # vali_data, vali_loader = data_provider(args, 'val')
+    test_data, test_loader = data_provider(args, 'test')
+
+    if args.model == 'Autoformer':
+        model = Autoformer.Model(args).float()
+    elif args.model == 'DLinear':
+        model = DLinear.Model(args).float()
+    else:
+        model = TimeLLM.Model(args).float()
+
+    args.content = load_content(args)
+
+    train_steps = len(train_loader)
+
+    trained_parameters = []
+    for p in model.parameters():
+        if p.requires_grad is True:
+            trained_parameters.append(p)
+
+    model_optim = optim.Adam(trained_parameters, lr=args.learning_rate)
+
+    if args.lradj == 'COS':
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(model_optim, T_max=20, eta_min=1e-8)
+    else:
+        scheduler = lr_scheduler.OneCycleLR(optimizer=model_optim,
+                                            steps_per_epoch=train_steps,
+                                            pct_start=args.pct_start,
+                                            epochs=args.train_epochs,
+                                            max_lr=args.learning_rate)
+
+    criterion = nn.MSELoss()
+    mae_metric = nn.L1Loss()
+
+    model.load_state_dict(torch.load(args.model_path, map_location=torch.device('cpu')))
+
+    test_loader, model, model_optim, scheduler= accelerator.prepare(test_loader, model, model_optim, scheduler)
+
+    # train_loader, vali_loader, test_loader, model, model_optim, scheduler = accelerator.prepare(
+    #     train_loader, vali_loader, test_loader, model, model_optim, scheduler)
+    
+    test_loss, test_mae_loss = vali(args, accelerator, model, test_data, test_loader, criterion, mae_metric)
+
+    accelerator.print("Test Loss: {0:.7f} MAE Loss: {1:.7f}".format(test_loss, test_mae_loss))
+
+
+accelerator.wait_for_everyone()
